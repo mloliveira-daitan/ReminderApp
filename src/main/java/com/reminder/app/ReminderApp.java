d7395ba8016280c30284662bfb2ea18ee8fd7b9e
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-06 09:18:10 -0300

    Testing sdk

--- src/main/java/com/reminder/app/ReminderApp.java
+++ src/main/java/com/reminder/app/ReminderApp.java
@@ -1,14 +1,28 @@
 package com.reminder.app;
 
 import com.bandwidth.sdk.*;
+import com.bandwidth.sdk.model.Call;
 
 public class ReminderApp
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static String toNumber = "+1530300777";// your phone number here
+    private static String fromNumber = "+1";// this is a number that is allocated on the AppPlatform. You can do this
+    // via the dev console or with the SDK (see AllocateNumberExample)
 
 
+    public static void main( String[] args ) throws Exception {
+
+
+
+
+        Call call = Call.create(toNumber, fromNumber);
+
+
+
+        Thread.sleep(7000);
+
+        call.hangUp();
 
 
     }
