2b1ae6b14815844de94f0e1df95b56fe1297a513
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-10 10:11:07 -0300

    servlet changes

--- src/main/java/com/reminder/app/Servlet.java
+++ src/main/java/com/reminder/app/Servlet.java
@@ -1,8 +1,22 @@
 package com.reminder.app;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Servlet extends HttpServlet{
 
+    protected void service (HttpServletRequest request,
+                            HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        // escreve o texto
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Primeira servlet");
+        out.println("</body>");
+        out.println("</html>");
+    }
 
 }
