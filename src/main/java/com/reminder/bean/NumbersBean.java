6f40c8a3cda841021d9ad454aac110b1c3e0237d
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-12 16:22:31 -0300

    UI wrapping

---
+++ src/main/java/com/reminder/bean/NumbersBean.java
@@ -0,0 +1,13 @@
+package com.reminder.bean;
+
+public class NumbersBean {
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
