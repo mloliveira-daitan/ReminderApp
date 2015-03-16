9d19abd69fbd52d59bf3e1bc87b3cb22f2f8d2d6
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-16 16:31:02 -0300

    working app

--- src/main/java/com/reminder/app/Main.java
+++ src/main/java/com/reminder/app/Main.java
@@ -7,6 +7,7 @@ import com.bandwidth.sdk.model.Gather;
 import com.bandwidth.sdk.model.Gender;
 import com.bandwidth.sdk.model.events.EventBase;
 import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -16,29 +17,17 @@ import java.util.Map;
 
 public class Main
 {
-
-    /*private static String userId = "u-jbg4qvzfcs6hpnq2mah7ona";
-    private static String apiToken =  "t-4iozqelr2fdj7amxjgz2o3y" ;
-    private static String apiSecret = "grkw7yze7dy4w7lpt2jdifnxhrhcmpjd7ft2xca";
-*/
-
     private static String userId = "u-m6vtffypexjt3k64ecumycy";
     private static String apiToken =  "t-tlq3f7nk2w5fjxre7zdmirq" ;
     private static String apiSecret = "buh23662yqwejlzohuqzpkouao22wirmhlrmgnq";
 
 
 
-   // BandwidthClient.setInstance(userId, apiToken, apiSecret);
-
-
-    private static String toNumber = "+15302987471";// your phone number here
-    private static String fromNumber = "+18595682277";// this is a number that is allocated on the AppPlatform. You can do this
-// via the dev console or with the SDK (see AllocateNumberExample)
 
 
     public static void main( String[] args ) throws Exception {
 
-        //BandwidthClient.getInstance().setEndpointandVersion("https://api.dev.catapult.inetwork.com", "v1");
+
         BandwidthClient.getInstance().setCredentials(userId, apiToken, apiSecret);
 
         String webappDirLocation = "src/main/webapp/";
@@ -71,22 +60,5 @@ public class Main
         server.join();
 
     }
-
-    protected static void waitForCallState(Call call) throws Exception {
-
-        int tries = 8;
-        while (!(call.getState() != null && call.getState().equals("active")) && tries > 0) {
-            tries--;
-            System.out.println(call.getState());
-            Thread.sleep(1250);
-        }
-        System.out.println(call.getState());
-        if (!(call.getState() != null && call.getState().equals("active"))) {
-            System.out.println("Call didn't change to active.");
-        }
-    }
-
-
-
 
 }
