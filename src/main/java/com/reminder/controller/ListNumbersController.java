dfc9e779fd54892c72b2174c1cfd4eb74e5e14d7
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-16 17:02:34 -0300

    working app

--- src/main/java/com/reminder/controller/ListNumbersController.java
+++ src/main/java/com/reminder/controller/ListNumbersController.java
@@ -16,13 +16,16 @@ import java.util.List;
 
 public class ListNumbersController extends HttpServlet {
 
+    private static String userId = "u-m6vtffypexjt3k64ecumycy";
+    private static String apiToken =  "t-tlq3f7nk2w5fjxre7zdmirq" ;
+    private static String apiSecret = "buh23662yqwejlzohuqzpkouao22wirmhlrmgnq";
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        BandwidthClient.getInstance();
+        BandwidthClient.getInstance().setCredentials(userId,apiToken,apiSecret);
 
         NumbersBean numbersBean = new NumbersBean();
         List<String> numberList = new ArrayList();
