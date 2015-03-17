17b7c24b0470b4136c5d64a58324563aefa71026
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-17 11:17:33 -0300

    Parameterizing app[4]

--- src/main/java/com/reminder/app/CallServlet.java
+++ src/main/java/com/reminder/app/CallServlet.java
@@ -15,7 +15,7 @@ public class CallServlet extends HttpServlet{
 
     private static String toNumber;
     private static String fromNumber;
-    private static String callbackUrl =  System.getenv("HEROKU_APP_NAME") + ".herokuapp.com/callback";
+    private static String callbackUrl =  "https://" + System.getenv("HEROKU_APP_NAME") + ".herokuapp.com/callback";
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 
@@ -35,7 +35,7 @@ public class CallServlet extends HttpServlet{
                 }
             }
 //TBD PARAMETER
-            Call call = Call.create(toNumber, fromNumber, callbackUrl, "testcall");
+            Call call = Call.create(toNumber, callbackUrlom/callback", "testcall");
             HttpSession session = req.getSession();
             session.setAttribute("callid", call.getId());
 
