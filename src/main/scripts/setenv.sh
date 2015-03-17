cc07ed510a70334da87c0f25659a68a7ecdfcbe0
Marco Oliveira <mloliveira@daitangroup.com>
2015-03-17 01:25:38 -0300

    Welcome page fix and readme.md done

--- src/main/scripts/setenv.sh
+++ src/main/scripts/setenv.sh
@@ -1,6 +1,11 @@
 
 export BANDWIDTH_API_ENDPOINT="https://api.catapult.inetwork.com"
 export BANDWIDTH_API_VERSION='v1'
-export BANDWIDTH_USER_ID='<your user id here>'
-export BANDWIDTH_API_TOKEN='<your api token here>'
-export BANDWIDTH_API_SECRET='<your api secret here>'
+#export BANDWIDTH_USER_ID='<your user id here>'
+#export BANDWIDTH_API_TOKEN='<your api token here>'
+#export BANDWIDTH_API_SECRET='<your api secret here>'
+
+export BANDWIDTH_USER_ID='u-m6vtffypexjt3k64ecumycy'
+export BANDWIDTH_API_TOKEN='t-tlq3f7nk2w5fjxre7zdmirq'
+export BANDWIDTH_API_SECRET='buh23662yqwejlzohuqzpkouao22wirmhlrmgnq'
+
